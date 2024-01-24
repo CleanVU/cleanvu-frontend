b104dd5f5f4c604b80fb34b99b9ff9f09fcce7a1
jacoblurie29 <jacoblurie29@gmail.com>
2024-01-24 15:02:30 -0600

    User interfaces updated

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,23 +2,8 @@ export interface User {
   _id: string;
   email: string;
   role: Role;
-}
-
-export interface Admin extends User {
-  role: Role.ADMIN;
-}
-
-export interface Student extends User {
-  building: string;
-  floor: number;
-  room: string;
-  role: Role.STUDENT;
-}
-
-export interface Custodian extends User {
-  building: string;
-  floors: number[];
-  role: Role.CUSTODIAN;
+  building?: string;
+  floor?: number;
 }
 
 export enum Role {
