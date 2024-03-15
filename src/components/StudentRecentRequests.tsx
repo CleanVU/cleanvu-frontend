0293599cffe68339f06945b3acb00a8a153e5351
sethamarx <seth.a.marx@vanderbilt.edu>
2024-03-14 21:52:20 -0400

    Updated dashboard

--- src/components/StudentRecentRequests.tsx
+++ src/components/StudentRecentRequests.tsx
@@ -1,4 +1,4 @@
-import { Card, Text } from "@mantine/core";
+import { Button, Card, Group, Text } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { getRequestByUserId } from "../api/api";
@@ -6,6 +6,8 @@ import { useRequestContext } from "../context/request.context";
 import { Request } from "../interfaces/request.interface";
 import StudentRequestCard from "./StudentRequestCard";
 import AddRequestModal from "./AddRequestModal";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface StudentRecentRequestsProps {
   userId: string;
@@ -39,8 +41,18 @@ const StudentRecentRequests = ({ userId }: StudentRecentRequestsProps) => {
 
   return (
     <>
-      <Card shadow="sm" padding="md" radius="md">
+      <Card shadow="sm" padding="xl">
         <Text>{`Recent Requests`}</Text>
+        <Button
+          variant="filled"
+          color="blue"
+          onClick={() => setAddRequestOpen(true)}
+        >
+          <Group gap={10}>
+            <Text fw={600}>Add Request</Text>
+            <FontAwesomeIcon icon={faPlus} />
+          </Group>
+        </Button>
         {recentRequests &&
           recentRequests.map((request) => (
             <StudentRequestCard key={request._id} request={request} />
