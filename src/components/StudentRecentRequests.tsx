1db21c0620e1a3d1667f654614ace4121d19d42c
sethamarx <seth.a.marx@vanderbilt.edu>
2024-03-15 14:44:12 -0400

    Modified dashboard

--- src/components/StudentRecentRequests.tsx
+++ src/components/StudentRecentRequests.tsx
@@ -1,13 +1,24 @@
-import { Button, Card, Group, Text, Title } from "@mantine/core";
+import {
+  Accordion,
+  Badge,
+  Button,
+  Card,
+  Divider,
+  Group,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { getRequestByUserId } from "../api/api";
 import { useRequestContext } from "../context/request.context";
-import { Request } from "../interfaces/request.interface";
-import StudentRequestCard from "./StudentRequestCard";
+import { Request, RequestStatusColors } from "../interfaces/request.interface";
 import AddRequestModal from "./AddRequestModal";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Location } from "../interfaces/location.interface";
+import { Building } from "../interfaces/building.interface";
 
 interface StudentRecentRequestsProps {
   userId: string;
@@ -42,7 +53,7 @@ const StudentRecentRequests = ({ userId }: StudentRecentRequestsProps) => {
   return (
     <>
       <Card shadow="sm" padding="xl">
-        <Group justify="space-between">
+        <Group justify="space-between" mb={10}>
           <Title order={3}>Recent Requests</Title>
           <Button
             variant="filled"
@@ -55,10 +66,82 @@ const StudentRecentRequests = ({ userId }: StudentRecentRequestsProps) => {
             </Group>
           </Button>
         </Group>
-        {recentRequests &&
-          recentRequests.map((request) => (
-            <StudentRequestCard key={request._id} request={request} />
-          ))}
+        <Divider />
+        {recentRequests && (
+          <Accordion>
+            {recentRequests.map((request) => (
+              <Accordion.Item key={request._id} value={request.description}>
+                <Accordion.Control>
+                  <Group
+                    justify="space-between"
+                    mt="md"
+                    mb="xs"
+                    ml="xs"
+                    mr="xs"
+                  >
+                    <Group gap={5}>
+                      <Title order={4} fw={400}>
+                        Request for
+                      </Title>
+                      <Title
+                        order={4}
+                      >{` ${(request.location as Location).description}`}</Title>
+                      <Title
+                        order={6}
+                        fw={200}
+                        style={{
+                          paddingTop: "2px",
+                        }}
+                      >{`(${new Date(request.createdAt).toLocaleString()})`}</Title>
+                    </Group>
+                    <Badge
+                      color={
+                        RequestStatusColors[
+                          request.status.toUpperCase() as keyof typeof RequestStatusColors
+                        ]
+                      }
+                    >
+                      {request.status}
+                    </Badge>
+                  </Group>
+                </Accordion.Control>
+                <Accordion.Panel>
+                  <Divider />
+                  <Stack gap={1}>
+                    <Group>
+                      <Text fw={600}>{`Building: `}</Text>
+                      <Text>{`${(request.building as Building).name}`}</Text>
+                    </Group>
+                    <Group>
+                      <Text fw={600}>{`Location: `}</Text>
+                      <Text>{`${(request.location as Location).name} (Floor ${(request.location as Location).floor})`}</Text>
+                    </Group>
+                    <Group>
+                      <Text fw={600}>{`Details:`}</Text>
+                      <Text>{`${request.description}`}</Text>
+                    </Group>
+                    <Group>
+                      <Text fw={600}>{`Initiated: `}</Text>
+                      <Text>{`${new Date(request.createdAt).toLocaleString()}`}</Text>
+                    </Group>
+                    {request.updatedAt && (
+                      <Group>
+                        <Text fw={600}>{`Last Updated: `}</Text>
+                        <Text>{`${new Date(request.updatedAt).toLocaleString()}`}</Text>
+                      </Group>
+                    )}
+                    {request.estimatedCompletion && (
+                      <Group>
+                        <Text fw={600}>{`Estimated Completion: `}</Text>
+                        <Text>{`${new Date(request.estimatedCompletion).toLocaleString()}`}</Text>
+                      </Group>
+                    )}
+                  </Stack>
+                </Accordion.Panel>
+              </Accordion.Item>
+            ))}
+          </Accordion>
+        )}
       </Card>
       {addRequestOpen && (
         <AddRequestModal
